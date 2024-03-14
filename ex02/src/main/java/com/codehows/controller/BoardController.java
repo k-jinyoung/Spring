f1980441e7112607d346367d4958046f2bb955f0
김진영 <154316245+k-jinyoung@users.noreply.github.com>
2024-03-14 14:45:47 +0900

    .

--- ex02/src/main/java/com/codehows/controller/BoardController.java
+++ ex02/src/main/java/com/codehows/controller/BoardController.java
@@ -3,7 +3,12 @@ package com.codehows.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.codehows.domain.BoardVO;
 import com.codehows.service.BoardService;
 
 import lombok.AllArgsConstructor;
@@ -22,6 +27,48 @@ public class BoardController {
 		
 		log.info("list");
 		model.addAttribute("list" , service.getList());
+	}
+	
+	@GetMapping("/register")
+	public void register() {
+		
+	}
+	
+	@PostMapping("/register")
+	public String register(BoardVO board, RedirectAttributes rttr) {
+		
+		log.info("register: " + board);
+		
+		service.register(board);
+		
+		rttr.addFlashAttribute("result", board.getBno());
+		
+		return "redirect:/board/list";
+	}
+	
+	@GetMapping("/get")
+	public void get(@RequestParam("bno") Long bno, Model model) {
+		log.info("/get");
+		model.addAttribute("board", service.get(bno));
+	}
+	
+	@PostMapping("/modify")
+	public String modify(BoardVO board, RedirectAttributes rttr) {
+		log.info("modify: " + board);
+		
+		if (service.modify(board)) {
+			rttr.addFlashAttribute("result", "success");
+		}
+		return "redirect:/board/list";
+	}
+	
+	@PostMapping("/remove")
+	public String remove(@RequestParam("bno") Long bno, RedirectAttributes rttr) {
+		log.info("remove..." + bno);
+		if (service.remove(bno)) {
+			rttr.addFlashAttribute("result", "success");
+		}
+		return "redirect:/board/list";
 	}
 
 }
